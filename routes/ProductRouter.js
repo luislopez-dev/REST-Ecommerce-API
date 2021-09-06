8320ceaf5b8dd5acaf924eb229809ecfffd1865e
Luis <luislopez100079@gmail.com>
2021-09-05 20:14:57 -0600

    Adding README info

--- routes/ProductRouter.js
+++ routes/ProductRouter.js
@@ -2,9 +2,6 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/ProductController');
 
-// Retrieve data from range of products
-router.post('/products', productController.getProducts);
-
 // Create a new product
 router.post('/', productController.addProduct);
 
@@ -16,5 +13,8 @@ router.delete('/:productId', productController.deleteProduct);
 
 // Update product ---- pending to fix
 router.put('', productController.editProduct);
+
+// Retrieve data from range of products
+router.post('/products', productController.getProducts);
 
 module.exports = router;
